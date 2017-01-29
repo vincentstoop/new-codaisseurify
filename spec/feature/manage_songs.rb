1e5aaf2a5b9ac6b9de9d3bbe575a5494652fa044
vincentstoop <vincentstoop@gmail.com>
2017-01-29 23:36:02 +0100

    First feature tests

---
+++ spec/feature/manage_songs.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'Manage songs', js: true do
+  let!(:artist1) {create :artist, name: 'John Doe'}
+
+  # scenario 'add a new song' do
+  #   visit artist_path artist1.id
+  #
+  #   fill_in 'input_title', with: 'Just a song'
+  #   fill_in 'input_year', with: '1993'
+  #
+  #   page.execute_script("$('form').submit()")
+  #
+  #   expect(page).to have_content('Just a song')
+  # end
+
+  scenario 'deletes a song' do
+    visit artist_path artist1.id
+
+    within '#songList' do
+      row = find('tr[data-song="1"]')
+      debugger
+      within row do
+        find('span.destroy').click
+      end
+      expect('#songList').not_to include("De Bestemming")
+    end
+  end
+end
