27591c4d3b55bbfea2d87cb702c018cec38cf481
vincentstoop <vincentstoop@gmail.com>
2017-01-29 09:38:13 +0100

    Add delete songs to ArtistController & Artist show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: 'artists#index'
-  resources :artists, only: [:show] do
+  resources :artists, only: [:show, :destroy] do
     resources :songs, only: [:create, :destroy]
   end
 
+  delete 'artist/:id/delete_all_songs', to: 'artists#delete_all_songs',
+                                      as: 'delete_songs'
   # resources :photos
 end
